ae0c9340c4c70f5b85dcc5ca3ff76e6357a3dc62
AyoubVD <79259209+AyoubVD@users.noreply.github.com>
2021-02-24 16:26:52 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,4 +6,3 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl daemon-reload
-sudo systemctl start mongod
