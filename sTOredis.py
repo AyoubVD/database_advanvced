1392d3668cee734670f79f2999c870b02f3f76bc
AyoubVD <79259209+AyoubVD@users.noreply.github.com>
2021-03-07 00:39:24 +0100

    Update sTOredis.py

--- sTOredis.py
+++ sTOredis.py
@@ -1,76 +1,90 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-from time import sleep
+# Import the neede packages
 import pandas as pd
+import numpy as np
+import pymongo as mongo
 import redis
+from bs4 import BeautifulSoup
+from requests import get
+from time import sleep
 
-my_url = "https://www.blockchain.com/btc/unconfirmed-transactions"
-uClient = uReq(my_url)
-page_html = uClient.read()
-uClient.close()
-page_soup = soup(page_html, "html.parser")
-containers = page_soup.find_all("div",{"class":"sc-1g6z4xm-0 hXyplo"})
+# Make connection to your database without security
+client = mongo.MongoClient("mongodb://127.0.0.1:27017")
 
-def maxValueIndex():
-    usd = []
+# Make a connection with redis
+connect = redis.Redis()
+
+# Make new list or arrays
+HashTable = []
+TimeTable = []
+BtcTable = []
+UsdTable = []
+
+# Make new list or arrays
+FilledHashTable = []
+FilledTimeTable = []
+FilledBtcTable = []
+FilledUsdTable = []
+
+# Connect to the existing names
+database = client["Database"]
+DataInBase = database["Data"]
+
+# Make a function
+
+def Scraper(HashTable, TimeTable, BtcTable, UsdTable, connect, DataInBase):
+
+    # The website you will be scraping
+    blockchain = get('https://www.blockchain.com/btc/unconfirmed-transactions')
+
+    # Put it into new variable
+    content = blockchain.content
+
+    # Use the Bs4 package to parse the website
+    soup = BeautifulSoup(content, "html.parser")
+    containers = soup.findAll('div', attrs={'class': 'sc-1g6z4xm-0 hXyplo'})
+
+    # The needed div you want to scrape
     for container in containers:
-        rawUsd = container.find_all('span',{'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
-        usd.append(rawUsd[2].text.replace('$', ''))
-    max_index = usd.index(max(usd))
-    return max_index
 
-def hash(x):
-    hashed = []
-    for container in containers:
-        rawHash = container.find_all('a',{'class':'sc-1r996ns-0 fLwyDF sc-1tbyx6t-1 kCGMTY iklhnl-0 eEewhk d53qjk-0 ctEFcK'})
-        hashed.append(rawHash[0].text)
-    mhashed = hashed[x]
-    return mhashed
+        # The needed classes you need
+        Hash = container.find('a', attrs={
+                      'class': 'sc-1r996ns-0 fLwyDF sc-1tbyx6t-1 kCGMTY iklhnl-0 eEewhk d53qjk-0 ctEFcK'})
+        Time = container.findAll('span', attrs={
+                         'class': 'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
+        btc = container.find('div', class_='sc-1au2w4e-0 fTyXWG')
 
-def time(x):
-    timed = []
-    for container in containers:
-        rawTime = container.find_all('span',{'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
-        timed.append(rawTime[0].text)
-    mtime = timed[x]
-    return mtime
+        btcConvert = float(btc.text[12:len(btc.text) - 3].strip())
 
-def BTC(x):
-    btc = []
-    for container in containers:
-        rawBtc = container.find_all('span',{'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
-        btc.append(rawBtc[1].text)
-    mbtc = btc[x]
-    return mbtc
+        usd = container.findAll('span', attrs={
+                        'class': 'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
+        usdConvert = float(usd[2].text[1:].replace(',', '').replace('$', ''))
 
-def USD(x):
-    usd = []
-    for container in containers:
-        rawUsd = container.find_all('span',{'class':'sc-1ryi78w-0 cILyoi sc-16b9dsl-1 ZwupP u3ufsr-0 eQTRKC'})
-        usd.append(rawUsd[2].text.replace('$', ''))
-    musd = usd[x]
-    return musd
+        # If the value is not found it will say it is unknown else it will show the output and push to Redis
+        
+        FilledHashTable.append(Hash.text)
+        connect.rpush("Hash", str(Hash.text))
 
-def calculatrix(df):
-    x = maxValueIndex()
-    dict = {"Hash":hash(x),"Time":time(x),"BTC":BTC(x),"USD":USD(x)}
-    df = df.append([dict], ignore_index=True,verify_integrity=True)
-    df.to_csv("blockchain.log", header="Hash", sep='\t', mode='a')
-    return df
+        FilledTimeTable.append(Time[0].text)
+        connect.rpush("Time", str(Time[0].text))
+        
+        FilledBtcTable.append(btcConvert)
+        connect.rpush("Amount(BTC)", str(btcConvert))
 
-x = maxValueIndex()
-dict = {"Hash":hash(x),"Time":time(x),"BTC":BTC(x),"USD":USD(x)}
-df = pd.DataFrame([dict])
-df.to_csv("blockchain.log", header="Hash", index=None, sep='\t', mode='a')
-print(df)
+        usdConvert = btcConvert * usdConvert
+        FilledUsdTable.append(usdConvert)
+        connect.rpush("Amount(USD)", str(usdConvert))
+
+    # Store the values and refresh every 60 seconds
+    connect.expire("Hash", 60)
+    connect.expire("Time", 60)
+    connect.expire("Amount(BTC)", 60)
+    connect.expire("Amount(USD)", 60)
+
 
 while True:
-    sleep(5)
-    uClient = uReq(my_url)
-    page_html = uClient.read()
-    uClient.close()
-    page_soup = soup(page_html, "html.parser")
-    containers = page_soup.find_all("div",{"class":"sc-1g6z4xm-0 hXyplo"})
-    df = calculatrix(df)
-    print(df)
 
+    # Call your function
+    Scraper(HashTable, TimeTable, BtcTable, UsdTable, connect, DataInBase)
+
+    # Send output every 60 seconds
+    sleep(60)
