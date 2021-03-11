04bd81ed66908bddfc7c1a5e9680070de2fd2f42
AyoubVD <79259209+AyoubVD@users.noreply.github.com>
2021-03-11 19:06:11 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -6,3 +6,6 @@ echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl daemon-reload
+sudo su
+docker pull redis
+docker pull mongo
